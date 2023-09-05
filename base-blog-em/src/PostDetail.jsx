462b15449af84778053420d27ea47c668e488d54
lee juyoung <juyung0903@naver.com>
2023-09-05 19:16:37 +0900

    useMutation사용

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 
 async function fetchComments(postId) {
   const response = await fetch(
@@ -29,6 +29,8 @@ export function PostDetail({ post }) {
     ["comments", post.id],
     () => fetchComments(post.id)
   );
+  const deleteMutation = useMutation((postId) => deletePost(postId));
+  const updateMutation = useMutation((postId) => updatePost(postId));
   if (isLoading) {
     return <div>loading...</div>;
   }
@@ -39,7 +41,22 @@ export function PostDetail({ post }) {
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={()=> deleteMutation.mutate(post.id)}>Delete</button> <button onClick={()=>updateMutation.mutate(post.id)}>Update title</button>
+      {
+        deleteMutation.isError && (
+          <p style={{color: "red"}}>Error deleting the post</p>
+        )
+      }
+      {
+        deleteMutation.isLoading && (
+          <p style={{color: "purple"}}>Deleting the Post</p>
+        )
+      }
+      {
+        deleteMutation.isSuccess && (
+          <p style={{color: "green"}}>Delete Success</p>
+        )
+      }
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data?.map((comment) => (
--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useQuery } from "react-query";
+import { useState, useEffect } from "react";
+import { useQuery, useQueryClient } from "react-query";
 import { PostDetail } from "./PostDetail";
 
 const maxPostPage = 10;
@@ -15,12 +15,25 @@ export function Posts() {
   const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      // 다음페이지의 데이터를 미리 가져옴
+      queryClient.prefetchQuery(["posts", nextPage], () =>
+        fetchPosts(nextPage)
+      );
+    }
+  }, [currentPage, queryClient]);
   // replace with useQuery
   const { data, isError, error, isLoading } = useQuery(
     ["posts", currentPage],
-    ()=> fetchPosts(currentPage),
+    () => fetchPosts(currentPage),
     {
       staleTime: 2000,
+      keepPreviousData:true,
+      //이전데이터를 계속남아있게 함으로써 로딩창이 뜨지않도록함
     }
   );
   if (isLoading) return <h3>Loading...</h3>;
@@ -44,15 +57,21 @@ export function Posts() {
         ))}
       </ul>
       <div className="pages">
-        <button disabled={currentPage <= 1} onClick={() => {
-          setCurrentPage((previousValue)=> previousValue-1)
-        }}>
+        <button
+          disabled={currentPage <= 1}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue - 1);
+          }}
+        >
           Previous page
         </button>
         <span>Page {currentPage + 1}</span>
-        <button disabled={currentPage >= maxPostPage} onClick={() => {
-          setCurrentPage((previousValue)=> previousValue+1)
-        }}>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => {
+            setCurrentPage((previousValue) => previousValue + 1);
+          }}
+        >
           Next page
         </button>
       </div>
