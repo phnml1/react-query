c359fedf0e68d11ff06615e9b042595bcf04a07a
lee juyoung <juyung0903@naver.com>
2023-09-07 16:01:33 +0900

    InfiniteScroll 사용하는 방법으로 구현

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,7 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
 import { useInfiniteQuery } from "react-query";
 import Person from "./Person";
-import { useEffect } from "react";
 const initialUrl = "https://swapi.dev/api/people/";
 const fetchUrl = async (url) => {
   const response = await fetch(url);
@@ -11,7 +10,7 @@ const fetchUrl = async (url) => {
 export function InfinitePeople() {
   // TODO: get data for InfiniteScroll via React Query
 
-  const { isSuccess, data, fetchNextPage, hasNextPage, isLoading, isError } =
+  const {  data, fetchNextPage, hasNextPage, isLoading, isFetching, isError } =
     useInfiniteQuery(
       "sw-people",
       ({ pageParam = initialUrl }) => fetchUrl(pageParam),
@@ -20,41 +19,26 @@ export function InfinitePeople() {
       }
     );
 
-  useEffect(() => {
-    let fetching = false;
-    const handleScroll = async (e) => {
-      const { scrollHeight, scrollTop, clientHeight } =
-        e.target.scrollingElement;
-      if (!fetching && scrollHeight - scrollTop <= clientHeight * 1.2) {
-        fetching = true;
-        if (hasNextPage) await fetchNextPage();
-        fetching = false;
-      }
-    };
-    document.addEventListener("scroll", handleScroll);
-    return () => {
-      document.removeEventListener("scroll", handleScroll);
-    };
-  }, [fetchNextPage, hasNextPage]);
+
   if (isLoading) return <div className="loading">Loading...</div>;
   if (isError) return <div className="error">error...</div>;
   console.log(data);
   return (
-    <div>
-      {isSuccess &&
-        data.pages.map((pageData) => 
-          pageData.results.map((person) => 
-            (
-              <div key={person.name} className="rrrr">
-                {person.name}
-                <ul>
-                  <li>hair: {person.hair_color}</li>
-                  <li>eyes: {person.eye_color}</li>
-                </ul>
-              </div>
-            )
-          )
+    <>
+    {isFetching && <div className="loading">Loading...</div>}
+    <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+      {
+        data.pages.map((pageData) =>
+          pageData.results.map((person) => (
+            <Person
+              key={person.name}
+              name={person.name}
+              hairColor={person.hair_color}
+              eyeColor={person.eye_color}
+            />
+          ))
         )}
-    </div>
+    </InfiniteScroll>
+    </>
   );
 }
